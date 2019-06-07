87c559eff0d7a03070ecba8012242156ea679986
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-07 01:41:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -67,7 +67,7 @@ class Owner
   end
 
   def list_pets
-    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), #{@pets[:cats].length} cat(s)."
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
   
 end
