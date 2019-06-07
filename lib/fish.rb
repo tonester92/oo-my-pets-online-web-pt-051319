95f6e951ce16d7833b3773873728356786b0561c
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-07 01:13:47 +0000

    Automatically backed up by Learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,10 @@
 class Fish
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+  end
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
-  # code goes here
+  attr_reader :species
+  attr_accessor :name, :pets, :fish, :dog, :cat
+
+  @@all = []
+  @@pets = {:fishes => [], :dogs => [], :cats => []}
+
+  def initialize(species)
+    @species = species
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count 
+    @@all.size
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+  
 end
